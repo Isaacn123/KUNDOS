ceb913351026441be5796e3c19fb1784198b0bbc
isaacn123 <nsambai72@gmail.com>
2022-10-04 23:44:12 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import {InAppBrowser} from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { SpinnerDialog } from '@awesome-cordova-plugins/spinner-dialog/ngx';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [InAppBrowser,SpinnerDialog,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {InAppBrowser,InAppBrowserOptions} from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { SpinnerDialog } from '@awesome-cordova-plugins/spinner-dialog/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iap: InAppBrowser, private spin: SpinnerDialog) {
+    const url  = 'https://kunde.4-raum.de/3d/';
+    this.mywebpage(url);
+  }
+
+
+  mywebpage(url){
+    const options: InAppBrowserOptions = {
+      hidden: 'no',
+      location: 'no',
+      zoom: 'yes',
+      hideurlbar: 'yes',
+    };
+    // this.spin.show();
+    const browser = this.iap.create(url,'_blank', options);
+      // browser.subscribe(() =>{
+      //   browser.insertCSS({ code: "body{color: red;" });
+      // })
+      browser.show();
+      // browser.on('loadstart').subscribe(() => {
+      //   this.spin.show();
+      //   }, err => {
+      //     this.spin.hide();
+      //   });
+
+      //   browser.on('loadstop').subscribe(()=>{
+      //   this.spin.hide();
+      //   }, err =>{
+      //  this.spin.hide();});
+      // browser.show();
+
+      // browser.close();
+  }
 
 }
